b7ece7b4054357eb0fb6d4c4fc296b6faeab026c
Festus K. Yangani <festus.yangani@gm.com>
2018-03-01 13:24:35 -0600

    Error handling for Alpha API server response

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -17,46 +17,52 @@ export class ApiService {
 
   //Add stock method
   addStock(ticker: string) {
-    let newStock: Stock = new Stock();
+    const newStock: Stock = new Stock();
 
     //Check for duplicates
-    if (this.stocks.find(obj => obj["ticker"] === ticker)) {
-      console.log("Duplicate");
+    if (this.stocks.find(obj => obj['ticker'] === ticker)) {
+      console.log('Duplicate');
       return this;
     }
 
     this.getStock(ticker)
-      .subscribe(res => {
-        let dateTime: string;
+      .subscribe(
+        res => {
+          let dateTime: string;
 
-        //Populate date object data;
-        newStock.ticker = res["Meta Data"]["2. Symbol"];
-        dateTime = res["Meta Data"]["3. Last Refreshed"];
-        newStock.date = dateTime.substr(0,10);
-        newStock.time = dateTime.substr(11);
-        newStock.price = res[ "Time Series (1min)"][dateTime]["4. close"];
+          //Populate date object data;
+          newStock.ticker = res['Meta Data']['2. Symbol'];
+          dateTime = res['Meta Data']['3. Last Refreshed'];
+          newStock.date = dateTime.substr(0, 10);
+          newStock.time = dateTime.substr(11);
+          newStock.price = res['Time Series (1min)'][dateTime]['4. close'];
 
-        //Add stock to stock list array
-        this.stocks.push(newStock);
-      });
+          //Add stock to stock list array
+          this.stocks.push(newStock);
+        },
+        error => {
+          alert(error.message);
+          throw new Error(error.message);
+        }
+      );
   }
 
 
   //Fetch stock from Alpha Vantage API
-  getStock(ticker: string){
-    let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=1min&apikey=8J157YWL897O9P5A`;
+  getStock(ticker: string) {
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=1min&apikey=8J157YWL897O9P5A`;
     return this.http.get(url);
   }
 
   getStockHistory(ticker: string) {
-    let url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&outputsize=compact&apikey=8J157YWL897O9P5A`;
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&outputsize=compact&apikey=8J157YWL897O9P5A`;
     return this.http.get(url);
   }
 
   //Delete stock method
   deleteStockMethod(ticker: string): ApiService {
     // Get index of stock to be deleted
-    let stockIndex = this.stocks.findIndex(obj => obj['ticker'] === ticker);
+    const stockIndex = this.stocks.findIndex(obj => obj['ticker'] === ticker);
     // Delete the stock from dataSource
     this.stocks.splice(stockIndex, 1);
 
